73ae349678f6c65660febf3d13021aedff4384f5
godcodegris <god-code@gmail.com>
2026-03-18 18:44:03 -0300

    temp: disable firebase for deployment

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,11 @@
-import admin from "firebase-admin";
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-const serviceAccount = require("../ecommerce-node-47022-firebase-adminsdk-fbsvc-c344eb26fc.json");
+//import admin from "firebase-admin";
+//import { createRequire } from "module";
+//const require = createRequire(import.meta.url);
+//const serviceAccount = require("../ecommerce-node-47022-firebase-adminsdk-fbsvc-c344eb26fc.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+//admin.initializeApp({
+  //credential: admin.credential.cert(serviceAccount)
+//});
 
-const db = admin.firestore();
-
-export { db };
+//const db = admin.firestore();
+export const db = null;
