3b734557230fb39cc1f2ecfe8c9db4ef71c3d6d6
godcodegris <god-code@gmail.com>
2026-03-19 17:47:31 -0300

    fix: mover dotenv.config antes de imports

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-dotenv.config();
-
 import usersRouter from "./routes/users.routes.js";
 import productsRouter from "./routes/products.routes.js";
-// import mlRouter from "./routes/mercadolibre.routes.js";
+import mlRouter from "./routes/mercadolibre.routes.js";
+
+dotenv.config();
 
 const app = express();
 
@@ -26,8 +26,8 @@ app.get("/", (req, res) => {
 
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
-// app.use("/mercadolibre", mlRouter);
-// app.use("/auth", mlRouter);
+app.use("/mercadolibre", mlRouter);
+app.use("/auth", mlRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
