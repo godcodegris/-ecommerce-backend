08a7132ab3ac40a0fe59da0f9ecd441b6a550176
godcodegris <god-code@gmail.com>
2026-03-21 14:23:03 -0300

    remove vercel api serverless folder

--- api/products.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    res.status(200).json([{ id: 1, name: 'Producto de ejemplo' }]);
-  } else {
-    res.status(405).end(); 
-  }
-}
